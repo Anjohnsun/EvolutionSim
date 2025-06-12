921bafd412d508facb84af99b64575c34c667939
Vya4eslav <vya4eslav.kruchin@yandex.ru>
2025-06-12 12:47:34 +0300

    Add data transfer interfaces, add DTO: individ, light source

---
+++ Assets/Interfaces/Dto/IndividDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Interfaces.Dto;
+
+public class IndividDto
+{
+    public int X { get; }
+    public int Y { get; }
+    public IndividDto(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
